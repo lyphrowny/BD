c027e1ce526d3ec2b63a78471c23e5d1dbd8e154
lyphrowny <strukov.as@edu.spbstu.ru>
2022-12-11 00:15:48 +0300

    lab (27) split on train/test; combined plots

--- l27.py
+++ l27.py
@@ -4,20 +4,44 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import matplotlib
 from sklearn import cluster, neighbors, naive_bayes
+from functools import reduce, partial
 
 
-def _make_cluster(xs, classification):
-    clust = defaultdict(list)
-    for x, clust_num in zip(xs, classification):
-        clust[clust_num].append(x)
-    return clust
+def _errors(res, orig):
+    c = defaultdict(list)
+    # otherwise the orig is exhausted after 1 iteration of the outer loop
+    orig = list(orig)
+    for cl, el in res:
+        for i, distr in orig:
+            if el in distr and i != cl:
+                c[(i, cl)].append(el)
+    return c
 
 
-def _plot(ttl, what):
-    for item in what.values():
-        plt.scatter(*zip(*item))
-    plt.grid()
-    plt.title(ttl)
+def __find_orig_order(c, _xs, thres):
+    swapped = {}
+    for k, v in c.items():
+        a, b = [swapped.get(_, _) for _ in k]
+        if len(v) > thres and swapped.get(a, None) != b:
+            swapped[a] = b
+            swapped[b] = a
+            _xs[a], _xs[b] = _xs[b], _xs[a]
+    return bool(swapped)
+
+
+def _make_dyn_dict(cl, dist):
+    return reduce(lambda g, el: g[el[0]].append(el[1]) or g, zip(cl, dist), defaultdict(list))
+
+
+def _plot_diff(res_cl, ttl, ttls, *, t_classes, t_dist):
+    c = _errors(zip(res_cl, t_dist), zip(t_classes, t_dist))
+    fig, axs = plt.subplots(1, 3, sharey=True, sharex=True)
+    fig.suptitle(ttl)
+    for ax, what, ttl in zip(axs, (_make_dyn_dict(t_classes, t_dist), _make_dyn_dict(res_cl, t_dist), c), ttls):
+        for lab, _what in what.items():
+            ax.scatter(*zip(*_what), label=lab)
+        ax.set_title(ttl)
+        ax.legend()
     plt.show()
 
 
@@ -33,24 +57,37 @@ def task():
 
     matplotlib.use('TkAgg')
     _xs = [stats.multivariate_normal.rvs(size=size, mean=mean, cov=cov) for mean, cov in zip(means, covs)]
-    _plot("true", {k: v for k, v in enumerate(_xs)})
     xs = np.array(_xs).reshape(-1, 2)
+    c_xs = xs.copy()
+
+    np.random.shuffle(xs)
+    _split = int(xs.shape[0] * .7)
+    train = xs[:_split]
+    test = xs[_split:]
+    # classification of test distribution
+    cl_test = np.where(test[:, None] == c_xs)[1][::2] // size
 
     cl_m = cluster.KMeans(n_clusters=n_clusters)
-    cl_m.fit(xs)
-    clust_pred = cl_m.predict(xs)
-    _plot("KMeans", _make_cluster(xs, clust_pred))
+    cl_m.fit(train)
+    # prediction to use as target values
+    train_classes = cl_m.predict(train)
+    test_classes = cl_m.predict(test)
 
-    y_classes = np.arange(n_clusters)[np.newaxis, ...].repeat(size, axis=1).flatten()
-    kn_model = neighbors.KNeighborsClassifier(n_neighbors=5)
-    kn_model.fit(X=xs, y=y_classes)
-    y_kn = kn_model.predict(xs)
-    _plot("KNeighbors", _make_cluster(xs, y_kn))
+    # remap the _xs classes; find the errors
+    while __find_orig_order((c := _errors(zip(cl_test, test), enumerate(_xs))), _xs, len(test) / n_clusters / 2):
+        ...
+    # print(c.keys(), *map(len, c.values()))
 
-    nb_model = naive_bayes.GaussianNB()
-    nb_model.fit(X=xs, y=y_classes)
-    y_nb = nb_model.predict(xs)
-    _plot("Bayesian", _make_cluster(xs, y_nb))
+    plot_diff = partial(_plot_diff, t_classes=test_classes, t_dist=test)
+    plot_diff(test_classes, "Test", "orig KMeans err".split())
+
+    for model, ttl, ttls in zip(
+            (neighbors.KNeighborsClassifier(n_neighbors=5), naive_bayes.GaussianNB()),
+            "KNeighbors Bayes".split(),
+            map(lambda s: s.split(), ("KMeans KNeighbors err", "KMeans Bayes err"))
+    ):
+        model.fit(X=train, y=train_classes)
+        plot_diff(model.predict(test), ttl, ttls)
 
 
 if __name__ == "__main__":
